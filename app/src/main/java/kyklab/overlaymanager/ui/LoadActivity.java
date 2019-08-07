79b662a66feec6e1fd8e5a05fe82be5094ab6dfd
maplezeon <kykint@naver.com>
2019-08-08 00:55:13 +0900

    Add andromeda access failure screen

--- app/src/main/java/kyklab/overlaymanager/ui/LoadActivity.java
+++ app/src/main/java/kyklab/overlaymanager/ui/LoadActivity.java
@@ -1,65 +1,33 @@
 package kyklab.overlaymanager.ui;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import kyklab.overlaymanager.R;
 import projekt.andromeda.client.AndromedaClient;
 
-public class LoadActivity extends AppCompatActivity {
+public class LoadActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int ANDROMEDA_REQ_CODE_PERMISSION = 14045;
-    private Context mContext;
-    private final Runnable serverInactiveRunnable = new Runnable() {
-        @Override
-        public void run() {
-            new AlertDialog.Builder(mContext)
-                    .setMessage("Andromeda crashed, probably Andromeda server is inactive at the moment.")
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            finish();
-                        }
-                    })
-                    .show();
-        }
-    };
-    private final Runnable startAppRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Log.e("ANDROMEDA", "run started");
-            try {
-                if (AndromedaClient.INSTANCE.isServerActive()) {
-                    Log.e("ANDROMEDA", "permission granted and server active");
-                    Intent intent = new Intent(mContext, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Log.e("ANDROMEDA", "permission granted and server inactive");
-                    runOnUiThread(serverInactiveRunnable);
-//                    new Thread(serverInactiveRunnable).start();
-                }
-            } catch (SecurityException e) {
-                e.printStackTrace();
-                Log.e("ANDROMEDA", "crashed while run");
-                runOnUiThread(serverInactiveRunnable);
-//                new Thread(serverInactiveRunnable).start();
-            }
-        }
-    };
+
+    private TextView andromedaWarning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
 
-        mContext = this;
+        andromedaWarning = findViewById(R.id.andromedaWarningBody);
+        ImageButton retryAndromeda = findViewById(R.id.retryAndromeda);
+        retryAndromeda.setOnClickListener(this);
 
         AndromedaClient.INSTANCE.initialize(this);
         if (ContextCompat.checkSelfPermission(this, AndromedaClient.ACCESS_PERMISSION)
@@ -67,15 +35,46 @@ public class LoadActivity extends AppCompatActivity {
             // Already has permission
             startAppIfAndromedaActive();
         } else {
-            requestPermissions(
-                    new String[]{AndromedaClient.ACCESS_PERMISSION},
-                    ANDROMEDA_REQ_CODE_PERMISSION
-            );
+            andromedaWarning.setText(R.string.andromeda_denied);
+            askAndromedaPermission();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.retryAndromeda:
+                askAndromedaPermission();
+                break;
+            default:
+                break;
         }
     }
 
     private void startAppIfAndromedaActive() {
-        new Thread(startAppRunnable).start();
+        Log.e("ANDROMEDA", "run started");
+        try {
+            if (AndromedaClient.INSTANCE.isServerActive()) {
+                Log.e("ANDROMEDA", "permission granted and server active");
+                Intent intent = new Intent(LoadActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Log.e("ANDROMEDA", "permission granted and server inactive");
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            Log.e("ANDROMEDA", "crashed while run");
+            andromedaWarning.setText(R.string.andromeda_crashed);
+        }
+    }
+
+    private void askAndromedaPermission() {
+        requestPermissions(
+                new String[]{AndromedaClient.ACCESS_PERMISSION},
+                ANDROMEDA_REQ_CODE_PERMISSION
+        );
     }
 
     @Override
@@ -84,17 +83,6 @@ public class LoadActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted
                 startAppIfAndromedaActive();
-            } else {
-                // Permission denied
-                new AlertDialog.Builder(this)
-                        .setMessage("Andromeda access permission denied")
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                finish();
-                            }
-                        })
-                        .show();
             }
         }
     }
